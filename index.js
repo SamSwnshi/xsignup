b84fdf45e919acbd08374b317afb2452b2e2f598
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-06-18 22:47:42 +0530

    new updated

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ document.getElementById("subscribe").addEventListener("submit",function(e){
     e.preventDefault()
     const email = document.getElementById('email').value;
     localStorage.setItem("SubscribeEmail",email)
-    window.location.href = "thank.html"
+    window.location.href = "success.html"
 })
